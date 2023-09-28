b1e38523f8cabbd7b1ab35cc8eb24f272e565c66
Fiona-Nyadero <fionanyadero@gmail.com>
2023-09-28 03:25:03 +0000

    Task 6 Corr

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -13,28 +13,22 @@ void counting_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
-
 	for (s = 0; s < size; s++)
 	{
 		if (array[s] > maximum)
 			maximum = array[s];
 	}
 	tally = malloc((maximum + 1) * sizeof(int));
-
 	if (tally == NULL)
 		return;
-
 	for (s = 0; s <= (size_t)maximum; s++)
 		tally[s] = 0;
-
 	for (s = 0; s < size; s++)
 		tally[array[s]]++;
-
 	for (s = 1; s <= (size_t)maximum; s++)
 		tally[s] += tally[s - 1];
 
 	sortd = malloc(size * sizeof(int));
-
 	if (sortd == NULL)
 	{
 		free(tally);
@@ -45,17 +39,11 @@ void counting_sort(int *array, size_t size)
 		sortd[tally[array[f]] - 1] = array[f];
 		tally[array[f]]--;
 	}
-	/*for (s = 0; s < size; s++)
-		array[s] = sortd[s];*/
-
+	for (s = 0; s < size; s++)
+		array[s] = sortd[s];
 	printf("Counting array:\n");
-
 	for (s = 0; s <= (size_t)maximum; s++)
 		printf("%d%s", tally[s], (s < (size_t)maximum) ? ", " : "\n");
-
-	for (s = 0; s < size; s++)
-                array[s] = sortd[s];
-
 	free(tally);
 	free(sortd);
 }
